bff816c68171f9f676ae9634efa43b9a93509133
Attila Szeremi <attila@szeremi.org>
2020-11-11 15:04:54 +0100

    Include chopped down example too

---
+++ choppedDownExport.js
@@ -0,0 +1,6 @@
+export const someProp1 = 'someValue1';
+export const someProp2 = 'someValue2';
+export const someProp3 = 'someValue3';
+export const someProp4 = 'someValue4';
+export const someProp5 = 'someValue5';
+export const someProp6 = 'someValue6';
--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+import {
+  someProp1,
+  someProp2,
+  someProp3,
+  someProp4,
+  someProp5,
+  someProp6,
+} from './choppedDownExport';
 import VeryLongJavascriptFileThatIsIntendedToBeImported from './VeryLongJavascriptFileThatIsIntendedToBeImported';
 
 console.info(VeryLongJavascriptFileThatIsIntendedToBeImported);
+console.info(someProp1, someProp2, someProp3, someProp4, someProp5, someProp6);
